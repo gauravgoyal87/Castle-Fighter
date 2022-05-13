d4a9b8555d8fdc2d8e7cca35cf51179810ea24a5
Gaurav <csogoyal@gmail.com>
2022-05-13 15:59:18 -0400

    artand script

---
+++ script.js
@@ -0,0 +1,29 @@
+// buttons and naming variables
+
+const fightBt= document.querySelector('#FIGHT')
+const buyBt= document.querySelector('#BUY')
+const testBt= document.querySelector('#class')
+const fighter1 = 0
+const fighter2 = 0
+let weapon1= 3
+let castleHP1= 20
+let castleHP2= 20
+let weapon2= 3
+let castleGold1 = 0
+let castleGold2 = 0
+let attack1 = 0
+let attack2 = 0
+testBt.innerHTML= attack1
+function attack1(){
+    attack1 += Math.ceil( Math.random * weapon1)
+}
+
+
+fightBt.addEventListener(war){
+    attacker1()
+    // attacker2()
+    // compare()
+
+}
+
+
